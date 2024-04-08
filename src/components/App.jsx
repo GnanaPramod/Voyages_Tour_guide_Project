c03514860f8a74aa4965faa89e9e95c69d2e9f8e
pradeep <118010705+Pradeep24032004@users.noreply.github.com>
2024-04-08 15:14:54 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,14 @@ import React, {useState, useEffect} from 'react';
 import Header from './Header';
 import Places from './Places';
 import axios from 'axios';
-import img1 from './imgs/gokarna.jpeg';
+/**import img1 from './imgs/gokarna.jpeg';
 import img2 from './imgs/charminar.jpg';
 import img3 from './imgs/araku.webp';
 import img4 from './imgs/ooty.jpeg';
 import img5 from './imgs/vizag.jpeg';
 import img6 from './imgs/kerala.jpg';
 import img7 from './imgs/manali.jpeg';
-import img8 from './imgs/pondicherry.webp';
+import img8 from './imgs/pondicherry.webp';**/
 import Slider from './Slider';
 import Footer from './Footer';
 function App() {
@@ -45,14 +45,7 @@ function App() {
             )}
           </div>
         ))}
-        <Places img={img1} name='Gokarna' info='Gokarna, Northwest Karnataka, India' />
-        <Places img={img2} name='Hyderabad' info='Hyderabad, Telangana, India' />
-        <Places img={img3} name='Araku' info='Araku, Andhra Pradesh, India' />
-        <Places img={img4} name='Ooty' info='Udagamandalam (Ooty), Tamil Nadu, India' /><br/>
-        <Places img={img5} name='Vizag' info='Visakhapatnam, Andhra Pradesh, India' />
-        <Places img={img6} name='Kerala' info='Kerala, India' />
-        <Places img={img7} name='Manali' info='Manali, Himachal Pradesh, India' />
-        <Places img={img8} name='Pondicherry' info='Pondicherry, India' />
+       
       </div>
       <Footer />
     </div>
@@ -60,3 +53,12 @@ function App() {
 }
 
 export default App;
+
+/**<Places img={img1} name='Gokarna' info='Gokarna, Northwest Karnataka, India' />
+        <Places img={img2} name='Hyderabad' info='Hyderabad, Telangana, India' />
+        <Places img={img3} name='Araku' info='Araku, Andhra Pradesh, India' />
+        <Places img={img4} name='Ooty' info='Udagamandalam (Ooty), Tamil Nadu, India' /><br/>
+        <Places img={img5} name='Vizag' info='Visakhapatnam, Andhra Pradesh, India' />
+        <Places img={img6} name='Kerala' info='Kerala, India' />
+        <Places img={img7} name='Manali' info='Manali, Himachal Pradesh, India' />
+        <Places img={img8} name='Pondicherry' info='Pondicherry, India' />**/
